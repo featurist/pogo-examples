b0bf0fe3ce56f4597053abc78a06c10fe9994bc3
joshski <joshuachisholm@gmail.com>
2012-03-04 12:31:19 +0000

    pogo highlighter keywords

--- highlight/pogoscript.js
+++ highlight/pogoscript.js
@@ -5,7 +5,7 @@ exports.pogoscript = function(hljs) {
   return {
     defaultMode: {
       keywords: {
-        'keyword': {'if': 1, 'for': 1, 'while': 1},
+        'keyword': {'if': 1, 'else': 1, 'for': 1, 'do': 1, 'while': 1, 'self': 1},
         'literal': {'true': 1, 'false': 1, 'null': 1}
       },
       contains: [
